c805c112c8ceb925caca3d475ac3fe3846b20e60
Colin Viebrock <c.viebrock@tippingcanoe.com>
2015-03-17 13:32:49 -0500

    namespace issue

--- src/GroupExtension.php
+++ src/GroupExtension.php
@@ -4,53 +4,49 @@ namespace Cviebrock\Twig;
 
 class GroupExtension extends \Twig_Extension {
 
-		/**
-		 * Return extension name
-		 *
-		 * @return string
-		 */
-		public function getName()
-		{
-			return 'cviebrock/group';
+	/**
+	 * Return extension name
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		return 'cviebrock/group';
+	}
+
+
+	/**
+	 * Callback for Twig
+	 *
+	 * @ignore
+	 */
+	public function getFilters() {
+		return [
+			'group' => new \Twig_Filter_Method($this, 'group')
+		];
+	}
+
+
+	public function group($array, $groups, $extraEntry = null) {
+		if ($array instanceof \Traversable) {
+			$array = iterator_to_array($array, false);
 		}
 
-
-		/**
-		 * Callback for Twig
-		 * @ignore
-		 */
-		public function getFilters()
-		{
-			return array(
-				'group' => new \Twig_Filter_Method($this, 'group')
-			);
+		// if moving into 1 (or fewer) groups, just return original array
+		if ($groups <= 1) {
+			return $arary;
 		}
 
-
-		public function group($array, $groups, $extraEntry = null)
-		{
-			if ($array instanceof Traversable) {
-				$array = iterator_to_array($array, false);
+		// add any extra entries, if needed
+		if ($extraEntry !== null) {
+			while (count($array) % $groups) {
+				$array[] = $extraEntry;
 			}
-
-			// if moving into 1 (or fewer) groups, just return original array
-			if ($groups <= 1 ) {
-				return $arary;
-			}
-
-			// add any extra entries, if needed
-			if ($extraEntry !== null) {
-				while ( count($array) % $groups ) {
-					$array[] = $extraEntry;
-				}
-			}
-
-			// calculate items per group
-			$perGroup = count($array) / $groups;
-
-			// chunk it
-			return array_chunk($array, $perGroup, true);
-
 		}
 
+		// calculate items per group
+		$perGroup = count($array) / $groups;
+
+		// chunk it
+		return array_chunk($array, $perGroup, true);
+	}
 }
